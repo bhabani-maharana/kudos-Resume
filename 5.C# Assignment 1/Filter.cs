747f3cf50ab6acbd197907fa62526a294681f740
bhabani-maharana <59605993+bhabani-maharana@users.noreply.github.com>
2020-02-13 15:18:06 +0530

    Add files via upload

---
+++ 5.C# Assignment 1/Filter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Filter
+    {
+        public void EvenFilter(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] % 2 == 0)
+                    Console.Write(arr[i] + ", ");
+        }
+
+        public void GreaterThanFilter(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] > 10)
+                    Console.Write(arr[i] + ", ");
+        }
+
+        public void DivByFilter(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] % 5 == 0)
+                    Console.Write(arr[i] + ", ");
+        }
+    }
+}
---
+++ 5.C# Assignment 1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public delegate void CallbackDelegate(int[] arr);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int i = 0;
+            
+            Console.WriteLine("Enter 10 Integer Values:");
+            while (i < arr.Length)
+            {
+                string val = Console.ReadLine();
+                int input;
+                if (int.TryParse(val, out input))
+                {
+                    arr[i] = input;
+                    i++;
+                }
+                else
+                        Console.WriteLine("Please Enter a Number\n");
+            }
+            while (true)
+            {
+                Console.WriteLine("1. Return all Even Numbers");
+                Console.WriteLine("2. Return numbers greater than 10");
+                Console.WriteLine("3. Return numbers divisible by 5");
+                Filter f = new Filter();
+                Console.Write(">>");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("-------------------");
+                switch (choice)
+                {
+                    case 1: GetResult(arr, f.EvenFilter);
+                        break;
+                    case 2: GetResult(arr, f.GreaterThanFilter);
+                        break;
+                    case 3: GetResult(arr, f.DivByFilter);
+                        break;
+                    default:
+                        Console.WriteLine("Select a Valid Option\n");
+                        break;
+                }
+            }
+            
+        }
+
+        static void GetResult(int[] arr, CallbackDelegate cbd) 
+        {
+            cbd(arr);
+            Console.WriteLine("\n-------------------");
+        }
+
+
+       
+    }
+}
